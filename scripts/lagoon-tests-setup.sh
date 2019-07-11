51528a2e89fb1075f000386c2a0f59465abf9bfc
sime <simon@hobbs.id.au>
2019-07-11 17:20:19 +1000

    Updated behat setup.

--- scripts/lagoon-tests-setup.sh
+++ scripts/lagoon-tests-setup.sh
@@ -2,11 +2,11 @@
 
 # Set up the dependencies for running full bootstrap tests in the lagoon container.
 
-composer install --ignore-platform-reqs
+copy .env.default .env
 export DOCKER_HOST=tcp://localhost:2375
 docker network prune -f && docker network create amazeeio-network
 docker login -u gitlab-ci-token -p $CI_JOB_TOKEN gitlab-registry-production.govcms.amazee.io
-docker-compose up
+docker-compose up -d
 docker-compose exec -T test dockerize -wait tcp://mariadb:3306 -timeout 1m
 
 if grep --quiet '^MARIADB_DATA_IMAGE' .env; then
