b78815d6cff164839b6692fa59593dab5759db9b
sime <simon@hobbs.id.au>
2019-07-12 00:11:57 +1000

    Update setup.

--- scripts/lagoon-tests-setup.sh
+++ scripts/lagoon-tests-setup.sh
@@ -2,7 +2,6 @@
 
 # Set up the dependencies for running full bootstrap tests in the lagoon container.
 
-copy .env.default .env
 export DOCKER_HOST=tcp://localhost:2375
 docker network prune -f && docker network create amazeeio-network
 docker login -u gitlab-ci-token -p $CI_JOB_TOKEN gitlab-registry-production.govcms.amazee.io
@@ -10,8 +9,8 @@ docker-compose up -d
 docker-compose exec -T test dockerize -wait tcp://mariadb:3306 -timeout 1m
 
 if grep --quiet '^MARIADB_DATA_IMAGE' .env; then
- ahoy -v lagoon govcms-deploy
+    ahoy -v lagoon govcms-deploy
 else
- ahoy -v govcms composer install
- ahoy -v govcms install
+    ahoy -v composer install
+    docker-compose exec -T test drush si -y "$@"
 fi
