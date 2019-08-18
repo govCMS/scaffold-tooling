f5bfb9d24674e36b8f83ad145e3f3df0e3792ec5
sime <simon@hobbs.id.au>
2019-08-18 15:22:59 +1000

    Add some info to full setup.

--- gitlab/setup-full.sh
+++ gitlab/setup-full.sh
@@ -16,12 +16,15 @@ if [[ $EXIT_CODE -ne 0 ]]; then
     echo "$MARIADB_DATA_IMAGE not found, installing GovCMS"
     docker-compose exec -T cli bash -c 'drush sql-drop'
     if [[ -f "./custom/database-quickstart.sql.gz" ]]; then
+        echo "Installing from a database dump."
         gunzip ./custom/database-quickstart.sql.gz
         docker-compose exec -T cli bash -c 'drush sql-cli --yes' < ./custom/database-quickstart.sql
     else
+        echo "Installing the default govcms profile."
         docker-compose exec -T cli bash -c 'drush si govcms -y'
     fi
 else
+    echo "Running govcms-deploy."
     ahoy govcms-deploy
 fi
 
