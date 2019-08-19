1a283c14e07aa914adf817f85ca155091916cb29
sime <simon@hobbs.id.au>
2019-08-19 16:20:52 +1000

    Fix var name.

--- gitlab/setup-static.sh
+++ gitlab/setup-static.sh
@@ -3,7 +3,7 @@ IFS=$'\n\t'
 set -exuo pipefail
 
 docker network prune -f && docker network create amazeeio-network
-docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $DOCKER_REGISTRY
+docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $GITLAB_DOCKER_REGISTRY
 
 # Only build cli/test containers for static testing.
 docker-compose up -d test
