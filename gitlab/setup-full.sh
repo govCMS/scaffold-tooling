c23449d66cc7c019055ffef3b29fa31f8c6f53d0
sime <simon@hobbs.id.au>
2019-08-20 16:29:17 +1000

    Use gitlab vars yo.

--- gitlab/setup-full.sh
+++ gitlab/setup-full.sh
@@ -3,17 +3,19 @@ IFS=$'\n\t'
 set -exuo pipefail
 
 docker network prune -f && docker network create amazeeio-network
-docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $GITLAB_DOCKER_REGISTRY
+docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $CI_REGISTRY
 
 docker-compose up -d
 docker-compose exec -T test dockerize -wait tcp://mariadb:3306 -timeout 1m
 
+DATABASE_IMAGE="$CI_REGISTRY_IMAGE/mariadb-drupal-data"
+
 # A hack to make the manifest check work.
 sed -i "s/^}$/,\"experimental\":\ \"enabled\"}/" ~/.docker/config.json
-EXIT_CODE=0 && docker manifest inspect "testagain" || EXIT_CODE=$?
+EXIT_CODE=0 && docker manifest inspect "$DATABASE_IMAGE" || EXIT_CODE=$?
 
 if [[ $EXIT_CODE -ne 0 ]]; then
-    echo "$MARIADB_DATA_IMAGE not found, installing GovCMS"
+    echo "$DATABASE_IMAGE not found, installing GovCMS"
     docker-compose exec -T cli bash -c 'drush sql-drop'
     if [[ -f "./custom/database-quickstart.sql.gz" ]]; then
         echo "Installing from a database dump."
