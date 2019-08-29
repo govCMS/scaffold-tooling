446419511e23f3633cef9b46d547bb9ef9cdc1fa
Alex Skrypnyk <alex.designworks@gmail.com>
2019-08-29 14:53:45 +1000

    Added CI configuration with linters.

---
+++ .circleci/phpcs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+targets=()
+while IFS=  read -r -d $'\0'; do
+    targets+=("$REPLY")
+done < <(
+  find \
+    drupal \
+    -type f \
+    -print0
+  )
+
+composer global require drupal/coder
+composer global require dealerdirect/phpcodesniffer-composer-installer
+
+for file in "${targets[@]}"; do
+  [ -f "${file}" ] && phpcs --standard=Drupal,DrupalPractice --colors "${file}"
+done;
