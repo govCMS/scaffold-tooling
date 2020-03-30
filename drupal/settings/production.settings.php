ab943c2d19a1ef8684545f87fc28bf1fa978c297
Steve <sj.worley88@gmail.com>
2020-03-30 16:04:38 +1000

    Ensure parity between settings.php (#35)
    * Update fast404 configuration.
    
    - Support env var override.
    - Add system/files for new private file handling.
    
    * Add ClamAV toggles.
    
    - Allows environment variables to override the clamav configuration.
    
    * Disable temporary file deletion.

--- drupal/settings/production.settings.php
+++ drupal/settings/production.settings.php
@@ -34,3 +34,6 @@ $config['system.performance']['js']['preprocess'] = 1;
 // Disabling stage file proxy on production, with that the module can be enabled
 // even on production.
 $config['stage_file_proxy.settings']['origin'] = FALSE;
+
+// Disable temporary file deletion (GOVCMSD8-576).
+$config['system.file']['temporary_maximum_age'] = 0;
