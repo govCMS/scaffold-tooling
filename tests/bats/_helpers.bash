28a48e365c1881f82a4ec8eacadcdd2d1cc8fd60
Alex Skrypnyk <alex@integratedexperts.com>
2020-04-21 18:08:32 +1000

    Added per-test temp directory.

--- tests/bats/_helpers.bash
+++ tests/bats/_helpers.bash
@@ -12,6 +12,22 @@ if [ -z "$TEST_PATH_INITIALIZED" ]; then
   PATH=/usr/bin:/usr/local/bin:/bin:/usr/sbin:/sbin
   # Add BATS test directory to the PATH.
   PATH="$(dirname "${BATS_TEST_DIRNAME}"):$PATH"
+
+  # BATS_TMPDIR - the location to a directory that may be used to store
+  # temporary files. Provided by bats. Created once for the duration of the
+  # whole suite run.
+  # Do not use BATS_TMPDIR, instead use BATS_TEST_TMPDIR.
+  #
+  # BATS_TEST_TMPDIR - unique location for temp files per test.
+  # shellcheck disable=SC2002
+  random_suffix=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
+  BATS_TEST_TMPDIR="${BATS_TMPDIR}/bats-test-tmp-${random_suffix}"
+  [ -d "${BATS_TEST_TMPDIR}" ] && rm -Rf "${BATS_TEST_TMPDIR}" > /dev/null
+  mkdir -p "${BATS_TEST_TMPDIR}"
+
+  export BATS_TEST_TMPDIR
+
+  echo "BATS_TEST_TMPDIR dir: ${BATS_TEST_TMPDIR}" >&3
 fi
 
 flunk(){
